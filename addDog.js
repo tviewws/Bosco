dfba2be6b31c06b86429e06a642887d254ed5ffb
derrickmomanyi <rickydee38@gmail.com>
2023-10-05 11:31:57 +0300

    Restructured the code

---
+++ addDog.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', ()=> {    
+    addDogs()
+  })
+
+
+const addDogs = () => {
+    let form = document.getElementById('form')
+    let name = document.getElementById('dogName')
+    let image = document.getElementById('dogImage')
+    let color = document.getElementById('dogColor')
+    let breed = document.getElementById('dogBreed')
+    let age = document.getElementById('dogAge')
+  
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
+  
+      const postDog = {
+        method: "POST",
+  
+        
+        body: JSON.stringify({
+          name: name.value,
+          breed: breed.value,
+          age: age.value,
+          color: color.value,
+          image: image.value
+        }),
+        headers: {
+                 'Content-Type': 'application/json'
+            
+               }
+      }
+  
+      fetch(' http://localhost:3000/dogs', postDog)
+      .then((res) => res.json())  
+      .then((data) => console.log(data))
+      window.location.href = 'index.html'
+          
+    
+    })
+  
+  }
+   
--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-  renderDogs()
-  addDogs()
+  fetchDogs()  
 })
 
-
-function renderDogs(){
-  fetch('db.json')
+const fetchDogs = () => {
+  fetch('http://localhost:3000/dogs')
   .then((response) => response.json())
-  .then((data) => {
-    const dogData = data.dogs;
+  .then((data) => renderDogs(data))
+}
+
+const renderDogs = (dogs) => {
+  dogs.forEach(dog => {
     const dogContainer = document.getElementById('dog-container');
-    document.getElementById('add-dog-button').addEventListener('click', function () {
+    const dogButton = document.getElementById('add-dog-button');
+        
+    dogButton.addEventListener('click', () => {
       window.location.href = 'add-dog.html'
     });
 
-    dogData.forEach((dog) => {
-      const card = document.createElement('div');
+    const card = document.createElement('div');
       card.classList.add('card');
 
       const name = document.createElement('h2');
@@ -49,48 +51,7 @@ function renderDogs(){
       });
 
       dogContainer.appendChild(card);
-    });
+
   })
-  // .catch((error) => {
-  //   console.error('Error fetching data:', error);
-  // });
-}
-
-
-const addDogs = () => {
-  let form = document.getElementById('form')
-  let name = document.getElementById('dogName')
-  let image = document.getElementById('dogImage')
-  let color = document.getElementById('dogColor')
-  let breed = document.getElementById('dogBreed')
-  let age = document.getElementById('dogAge')
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    const postDog = {
-      method: "POST",
-
-      
-      body: JSON.stringify({
-        name: name.value,
-        breed: breed.value,
-        age: age.value,
-        color: color.value,
-        image: image.value
-      }),
-      headers: {
-               'Content-Type': 'application/json'
-          
-             }
-    }
-
-    fetch(' http://localhost:3000/dogs', postDog)
-    .then((res) => res.json())  
-    .then((data) => console.log(data))
-        
   
-  })
-
 }
- 
